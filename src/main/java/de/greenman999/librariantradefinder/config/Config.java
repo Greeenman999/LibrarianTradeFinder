e0557ac3c7226356091a7f4a8041d79a70711b3e
Greenman999 <git@greenman999.de>
2025-12-21 17:23:22 +0100

    feat: create initial configuration

--- src/main/java/de/greenman999/librariantradefinder/config/Config.java
+++ src/main/java/de/greenman999/librariantradefinder/config/Config.java
@@ -19,9 +19,159 @@
 
 package de.greenman999.librariantradefinder.config;
 
+import de.greenman999.librariantradefinder.util.IntegerRange;
+import org.jspecify.annotations.Nullable;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
 @ConfigSerializable
 public class Config {
 
+	/**
+	 * When in BREAKING phase, stop search if the tool has less than 5 durability left
+	 */
+	private boolean preventToolBreaking = true;
+
+	/**
+	 * Teleport to dropped items (e.g. lecterns) between BREAKING and PLACING phase
+	 * to automatically pick items up that dropped behind the player or lectern
+	 */
+	private boolean teleportToDroppedItems = false;
+
+	/**
+	 * Before interacting with the world (breaking/placing/checking villager),
+	 * look at the target block/entity to prevent bugs.
+	 * Look back to original position after the interaction.
+	 */
+	private boolean smartLookMode = true;
+
+	/**
+	 * Slow down the search process to make it more human-like
+	 * and less likely to be detected as a bot.
+	 * This is done by lerping the look movements and adding small delays
+	 * between actions.
+	 */
+	private boolean slowMode = false;
+
+	/**
+	 * The range of ticks to delay between actions in slow mode.
+	 * Minimum is inclusive, maximum is inclusive.
+	 * Picked randomly per action.
+	 */
+	private IntegerRange actionDelayTicks = new IntegerRange(1, 3);
+
+	/**
+	 * Automatically buy the trade when a matching trade is found.
+	 * This should lock the trade.
+	 */
+	private boolean buyOnTradeFound = false;
+
+	/**
+	 * Notify the player with a sound when a matching trade is found.
+	 */
+	private boolean notifyOnTradeFound = true;
+
+	/**
+	 * The type of second trade to look for.
+	 * If null, only the enchanted book trade is considered.
+	 */
+	@Nullable
+	private SecondTradeType secondTradeType = null;
+
+	public enum SecondTradeType {
+		SELL_PAPER,
+		BUY_BOOKSHELF,
+	}
+
+	/**
+	 * Display the trades in front of the villager.
+	 */
+	private boolean displayTradesOnVillager = false;
+
+	/**
+	 * The timeout in ticks to wait after PLACING phase
+	 * if the villager does not take the job.
+	 * If the timeout is reached, the mod will switch to BREAKING phase
+	 * to reroll the villager.
+	 */
+	private int rerollTimeoutTicks = 100;
+
+	public boolean shouldPreventToolBreaking() {
+		return preventToolBreaking;
+	}
+
+	public void setPreventToolBreaking(boolean preventToolBreaking) {
+		this.preventToolBreaking = preventToolBreaking;
+	}
+
+	public boolean shouldTeleportToDroppedItems() {
+		return teleportToDroppedItems;
+	}
+
+	public void setTeleportToDroppedItems(boolean teleportToDroppedItems) {
+		this.teleportToDroppedItems = teleportToDroppedItems;
+	}
+
+	public boolean isSmartLookMode() {
+		return smartLookMode;
+	}
+
+	public void setSmartLookMode(boolean smartLookMode) {
+		this.smartLookMode = smartLookMode;
+	}
+
+	public boolean isSlowMode() {
+		return slowMode;
+	}
+
+	public void setSlowMode(boolean slowMode) {
+		this.slowMode = slowMode;
+	}
+
+	public IntegerRange getActionDelayTicks() {
+		return actionDelayTicks;
+	}
+
+	public void setActionDelayTicks(IntegerRange actionDelayTicks) {
+		this.actionDelayTicks = actionDelayTicks;
+	}
+
+	public boolean shouldBuyOnTradeFound() {
+		return buyOnTradeFound;
+	}
+
+	public void setBuyOnTradeFound(boolean buyOnTradeFound) {
+		this.buyOnTradeFound = buyOnTradeFound;
+	}
+
+	public boolean shouldNotifyOnTradeFound() {
+		return notifyOnTradeFound;
+	}
+
+	public void setNotifyOnTradeFound(boolean notifyOnTradeFound) {
+		this.notifyOnTradeFound = notifyOnTradeFound;
+	}
+
+	public @Nullable SecondTradeType getSecondTradeType() {
+		return secondTradeType;
+	}
+
+	public void setSecondTradeType(@Nullable SecondTradeType secondTradeType) {
+		this.secondTradeType = secondTradeType;
+	}
+
+	public boolean shouldDisplayTradesOnVillager() {
+		return displayTradesOnVillager;
+	}
+
+	public void setDisplayTradesOnVillager(boolean displayTradesOnVillager) {
+		this.displayTradesOnVillager = displayTradesOnVillager;
+	}
+
+	public int getRerollTimeoutTicks() {
+		return rerollTimeoutTicks;
+	}
+
+	public void setRerollTimeoutTicks(int rerollTimeoutTicks) {
+		this.rerollTimeoutTicks = rerollTimeoutTicks;
+	}
 }
