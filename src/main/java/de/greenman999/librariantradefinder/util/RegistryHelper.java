d52dc14d08f40b218c4a20a6d8f3747057849fb9
Greenman999 <git@greenman999.de>
2025-12-22 13:23:20 +0100

    feat: add RegistryHelper for enchantment retrieval

---
+++ src/main/java/de/greenman999/librariantradefinder/util/RegistryHelper.java
@@ -0,0 +1,44 @@
+/*
+ * A minecraft mod that helps you find the enchantments you need from a Librarian Villager.
+ * Copyright (C) 2025. Greenman999
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package de.greenman999.librariantradefinder.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.enchantment.Enchantment;
+
+public class RegistryHelper {
+
+	public static Registry<Enchantment> getEnchantmentRegistry() {
+		if (Minecraft.getInstance().level == null) {
+			return null;
+		}
+		return Minecraft.getInstance().level.registryAccess().lookupOrThrow(Registries.ENCHANTMENT);
+	}
+
+	public static Enchantment getEnchantmentById(Identifier id) {
+		Registry<Enchantment> enchantmentRegistry = getEnchantmentRegistry();
+		if (enchantmentRegistry == null || !enchantmentRegistry.containsKey(id) || enchantmentRegistry.get(id).isEmpty())
+			return null;
+		return enchantmentRegistry.get(id).get().value();
+	}
+
+}
