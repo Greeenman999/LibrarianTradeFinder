f8c06a44599832a4c322a62085cb935e8678dc03
Greenman999 <git@greenman999.de>
2025-12-22 13:24:54 +0100

    feat: register custom configuration screen for librarian trade finder on neoforge (not working yet)

--- src/main/java/de/greenman999/librariantradefinder/platform/neoforge/NeoforgeEntrypoint.java
+++ src/main/java/de/greenman999/librariantradefinder/platform/neoforge/NeoforgeEntrypoint.java
@@ -9,13 +9,30 @@ package de.greenman999.librariantradefinder.platform.neoforge;
 //? neoforge {
 
 /*import de.greenman999.librariantradefinder.LibrarianTradeFinder;
+import de.greenman999.librariantradefinder.gui.ConfigScreen;
+import gg.essential.universal.UResolution;
+import gg.essential.universal.UScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.options.OptionsScreen;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(LibrarianTradeFinder.MOD_ID)
 public class NeoforgeEntrypoint {
 
 	public NeoforgeEntrypoint() {
 		LibrarianTradeFinder.onInitialize();
+		ModLoadingContext.get().registerExtensionPoint(
+				IConfigScreenFactory.class,
+				() -> (clientGui, parent) -> {
+					parent.onClose();
+					Screen screen = new ConfigScreen(parent);
+					screen.init(UResolution.getScaledWidth(), UResolution.getScaledHeight());
+					return screen;
+				}
+		);
 	}
 }
 *///?}
