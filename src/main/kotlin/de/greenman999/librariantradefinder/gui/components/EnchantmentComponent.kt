05a30d9ac0255a42ab54d53f26ecd0e91bb4e3e9
Greenman999 <git@greenman999.de>
2025-12-31 00:26:57 +0100

    refactor: move components in EnchantmentComponent out of initializer and add reset method to reset the enchantment

--- src/main/kotlin/de/greenman999/librariantradefinder/gui/components/EnchantmentComponent.kt
+++ src/main/kotlin/de/greenman999/librariantradefinder/gui/components/EnchantmentComponent.kt
@@ -46,10 +46,96 @@ import java.awt.Color
 class EnchantmentComponent(val entry: MutableMap.MutableEntry<Identifier, Config.EnchantmentEntry>, searchTextState: BasicState<String>, fuzzyScore: FuzzyScore) : UIRoundedRectangle(4f) {
 
 	val colorState: BasicState<Color> = BasicState(Color(0, 0, 0, 100))
+	val config: Config = LibrarianTradeFinder.getInstance().config
+
+	val enchantment: Enchantment = RegistryHelper.getEnchantmentById(entry.key)
+	fun maxEmeraldCost(level: Int? = entry.value.level) = RegistryHelper.getMaxEmeraldCost(entry.key, level)
+	fun minEmeraldCost(level: Int? = entry.value.level) = RegistryHelper.getMinEmeraldCost(entry.key, level)
+	fun denormalize(value: Float, min: Int, max: Int) = (value * (max - min)) + min
+	fun normalize(value: Int, min: Int, max: Int) = if (min == max) {
+		min.toFloat()
+	} else {
+		(value - min).toFloat() / (max - min).toFloat()
+	}
+	fun calculateEmeraldsFromValue(value: Float) = denormalize(value, minEmeraldCost(), maxEmeraldCost())
+	fun normalizeEmeralds(emeralds: Int) = normalize(emeralds, minEmeraldCost(), maxEmeraldCost())
+	fun calculateLevelFromValue(value: Float) = denormalize(value, enchantment.minLevel, enchantment.maxLevel).toInt()
+	fun normalizeLevel(level: Int) = normalize(level, enchantment.minLevel, enchantment.maxLevel)
+
+	val name by UIText(enchantment.description().string).constrain {
+		x = 5.pixels()
+		y = CenterConstraint()
+
+		color = Color.WHITE.toConstraint()
+	} childOf this
+
+	val emeraldsSlider = SliderComponent(normalizeEmeralds(entry.value.maxPrice)).constrain {
+		x = 5.pixels(alignOpposite = true)
+		y = CenterConstraint()
+
+		width = 50.pixels()
+		height = 14.pixels()
+	}.onValueSave {
+		if (!config.isEnchantmentEnabled(entry.key)) return@onValueSave
+
+		val newValue = calculateEmeraldsFromValue(it).toInt()
+		entry.value.maxPrice = newValue
+
+		config.setMaxPriceForEnchantment(entry.key, newValue)
+		LibrarianTradeFinder.getInstance().configManager.save()
+	}.formatText { value ->
+		(calculateEmeraldsFromValue(value)).toInt().toString()
+	} childOf this
+
+	val emeraldTooltip by TooltipComponent(emeraldsSlider)
+		.bindVisibility(emeraldsSlider)
+		.bindText(BasicState(translatable("librariantradefinder.gui.tooltip.max-price")))
+
+	val levelSlider = SliderComponent(normalizeLevel(entry.value.level)).constrain {
+		x = 5.pixels(alignOpposite = true) - 50.pixels() - 5.pixels()
+		y = CenterConstraint()
+
+		width = 40.pixels()
+		height = 14.pixels()
+	}.onValueChange {
+		if (!config.isEnchantmentEnabled(entry.key)) return@onValueChange
+
+		val newLevel = calculateLevelFromValue(it)
+		entry.value.level = newLevel
+		// Update emeralds slider in case min level change affected costs
+		if (entry.value.maxPrice < minEmeraldCost(newLevel)) {
+			entry.value.maxPrice = minEmeraldCost(newLevel)
+		} else if (entry.value.maxPrice > maxEmeraldCost(newLevel)) {
+			entry.value.maxPrice = maxEmeraldCost(newLevel)
+		}
+
+		emeraldsSlider.updateSliderValue(normalizeEmeralds(entry.value.maxPrice))
+	}.onValueSave {
+		if (!config.isEnchantmentEnabled(entry.key)) return@onValueSave
+
+		config.setLevelForEnchantment(entry.key, entry.value.level)
+		LibrarianTradeFinder.getInstance().configManager.save()
+	}.formatText {
+		when (calculateLevelFromValue(it)) {
+			1 -> "I"
+			2 -> "II"
+			3 -> "III"
+			4 -> "IV"
+			5 -> "V"
+			6 -> "VI"
+			7 -> "VII"
+			8 -> "VIII"
+			9 -> "IX"
+			10 -> "X"
+			else -> calculateLevelFromValue(it).toString()
+		}
+	} childOf this
+
+	val levelTooltip by TooltipComponent(levelSlider)
+		.bindVisibility(levelSlider)
+		.bindText(BasicState(translatable("librariantradefinder.gui.tooltip.level")))
 
 	init {
-		val config: Config = LibrarianTradeFinder.getInstance().config
-
 		constrain {
 			x = 0.pixels()
 			y = SiblingConstraint(padding = 2f)
@@ -59,117 +145,7 @@ class EnchantmentComponent(val entry: MutableMap.MutableEntry<Identifier, Config
 			width = 100.percent()
 			height = 20.pixels()
 		}
-
-		val enchantment: Enchantment = RegistryHelper.getEnchantmentById(entry.key)
-		fun maxEmeraldCost(level: Int? = entry.value.level) = RegistryHelper.getMaxEmeraldCost(entry.key, level)
-		fun minEmeraldCost(level: Int? = entry.value.level) = RegistryHelper.getMinEmeraldCost(entry.key, level)
-		fun denormalize(value: Float, min: Int, max: Int) = (value * (max - min)) + min
-		fun normalize(value: Int, min: Int, max: Int) = if (min == max) {
-			min.toFloat()
-		} else {
-			(value - min).toFloat() / (max - min).toFloat()
-		}
-		fun calculateEmeraldsFromValue(value: Float) = denormalize(value, minEmeraldCost(), maxEmeraldCost())
-		fun normalizeEmeralds(emeralds: Int) = normalize(emeralds, minEmeraldCost(), maxEmeraldCost())
-		fun calculateLevelFromValue(value: Float) = denormalize(value, enchantment.minLevel, enchantment.maxLevel).toInt()
-		fun normalizeLevel(level: Int) = normalize(level, enchantment.minLevel, enchantment.maxLevel)
-
-		val name by UIText(enchantment.description().string).constrain {
-			x = 5.pixels()
-			y = CenterConstraint()
-
-			color = Color.WHITE.toConstraint()
-		} childOf this
-
-		val emeraldsSlider = SliderComponent(normalizeEmeralds(entry.value.maxPrice)).constrain {
-			x = 5.pixels(alignOpposite = true)
-			y = CenterConstraint()
-
-			width = 50.pixels()
-			height = 14.pixels()
-		}.onValueSave {
-			if (!config.isEnchantmentEnabled(entry.key)) return@onValueSave
-
-			val newValue = calculateEmeraldsFromValue(it).toInt()
-			entry.value.maxPrice = newValue
-
-			config.setMaxPriceForEnchantment(entry.key, newValue)
-			LibrarianTradeFinder.getInstance().configManager.save()
-		}.formatText { value ->
-			(calculateEmeraldsFromValue(value)).toInt().toString()
-		} childOf this
-
-		val emeraldTooltip by TooltipComponent(emeraldsSlider)
-			.bindVisibility(emeraldsSlider)
-			.bindText(BasicState(translatable("librariantradefinder.gui.tooltip.max-price")))
-
-		val levelSlider = SliderComponent(normalizeLevel(entry.value.level)).constrain {
-			x = 5.pixels(alignOpposite = true) - 50.pixels() - 5.pixels()
-			y = CenterConstraint()
-
-			width = 40.pixels()
-			height = 14.pixels()
-		}.onValueChange {
-			if (!config.isEnchantmentEnabled(entry.key)) return@onValueChange
-
-			val newLevel = calculateLevelFromValue(it)
-			entry.value.level = newLevel
-			// Update emeralds slider in case min level change affected costs
-			if (entry.value.maxPrice < minEmeraldCost(newLevel)) {
-				entry.value.maxPrice = minEmeraldCost(newLevel)
-			} else if (entry.value.maxPrice > maxEmeraldCost(newLevel)) {
-				entry.value.maxPrice = maxEmeraldCost(newLevel)
-			}
-
-			emeraldsSlider.updateSliderValue(normalizeEmeralds(entry.value.maxPrice))
-		}.onValueSave {
-			if (!config.isEnchantmentEnabled(entry.key)) return@onValueSave
-
-			config.setLevelForEnchantment(entry.key, entry.value.level)
-			LibrarianTradeFinder.getInstance().configManager.save()
-		}.formatText {
-			when (calculateLevelFromValue(it)) {
-				1 -> "I"
-				2 -> "II"
-				3 -> "III"
-				4 -> "IV"
-				5 -> "V"
-				6 -> "VI"
-				7 -> "VII"
-				8 -> "VIII"
-				9 -> "IX"
-				10 -> "X"
-				else -> calculateLevelFromValue(it).toString()
-			}
- 		} childOf this
-
-		val levelTooltip by TooltipComponent(levelSlider)
-			.bindVisibility(levelSlider)
-			.bindText(BasicState(translatable("librariantradefinder.gui.tooltip.level")))
-
 		withHandCursor()
-
-		fun updateEntry(toggle: Boolean) {
-			if (toggle) {
-				config.enableEnchantment(entry.key, !config.isEnchantmentEnabled(entry.key))
-				LibrarianTradeFinder.getInstance().configManager.save()
-			}
-
-			colorState.set {
-				if (config.isEnchantmentEnabled(entry.key)) {
-					Color(56, 171, 80, 150)
-				} else {
-					Color(0, 0, 0, 100)
-				}
-			}
-			if (config.isEnchantmentEnabled(entry.key)) {
-				emeraldsSlider.unhide(true)
-				levelSlider.unhide(true)
-			} else {
-				emeraldsSlider.hide(false)
-				levelSlider.hide(false)
-			}
-		}
 
 		onMouseClick {
 			updateEntry(true)
@@ -194,6 +170,36 @@ class EnchantmentComponent(val entry: MutableMap.MutableEntry<Identifier, Config
 		}
 	}
 
+	fun updateEntry(toggle: Boolean) {
+		println("Updating entry for ${enchantment.description().string}, toggle=$toggle")
+		if (toggle) {
+			config.enableEnchantment(entry.key, !config.isEnchantmentEnabled(entry.key))
+			LibrarianTradeFinder.getInstance().configManager.save()
+		}
 
+		colorState.set {
+			println("Setting color for ${enchantment.description().string}, enabled=${config.isEnchantmentEnabled(entry.key)}")
+			if (config.isEnchantmentEnabled(entry.key)) {
+				Color(56, 171, 80, 150)
+			} else {
+				Color(0, 0, 0, 100)
+			}
+		}
+		if (config.isEnchantmentEnabled(entry.key)) {
+			emeraldsSlider.unhide(true)
+			levelSlider.unhide(true)
+		} else {
+			emeraldsSlider.hide(false)
+			levelSlider.hide(false)
+		}
+	}
 
+	fun reset() {
+		entry.setValue(Config.EnchantmentEntry(
+			false,
+			RegistryHelper.getEnchantmentById(entry.key).maxLevel,
+			RegistryHelper.getMaxEmeraldCost(entry.key, RegistryHelper.getEnchantmentById(entry.key).maxLevel)
+		))
+		updateEntry(false)
+	}
 }
